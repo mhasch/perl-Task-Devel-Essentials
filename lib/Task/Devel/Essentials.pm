08984c401eac13ad1bce5ea1d8ee20b896035740
Martin Becker <mhasch@cpan.org>
2016-09-02 14:47:00 +0200

    initial commit, v0.003, of 2014-05-30

---
+++ lib/Task/Devel/Essentials.pm
@@ -0,0 +1,44 @@
+package Task::Devel::Essentials;
+
+use strict;
+
+=head1 NAME
+
+Task::Devel::Essentials - Bundle of modules useful for developers
+
+=head1 VERSION
+
+Version 0.003
+
+=cut
+
+our $VERSION = '0.003';
+
+=head1 SYNOPSIS
+
+  # to install a couple of modules useful for developers in one go,
+  # execute one of the commands below:
+
+  cpan Task::Devel::Essentials
+  cpanm Task::Devel::Essentials
+  cpanp i Task::Devel::Essentials
+
+=head1 AUTHOR
+
+Martin Becker, C<< <mhasch at cpan.org> >>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2013-2014 Martin Becker.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+=cut
+
+1;
+
+__END__
